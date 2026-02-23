aafe70201605a2e29692f73243192b8b3a8cb602
Ilya <ilya.kalmykov.2006@gmail.com>
2026-02-23 14:43:39 +0300

    fix port for render

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 /* ===============================
    MULTER (загрузка изображений)
@@ -366,6 +366,6 @@ app.delete("/compare/:productId", async (req, res) => {
   }
 });
 
-app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
